42933a7ad7dfd6aa3000cd271712377afe79edd6
Bahar <156964654+okimoben@users.noreply.github.com>
2025-04-10 15:07:47 +0300

    Create manipulate.py

---
+++ string_package/manipulate.py
@@ -0,0 +1,16 @@
+import string
+
+def reverse_string(s):
+    return s[::-1]
+
+def capitalize_words(s):
+    return ' '.join(word.capitalize() for word in s.split())
+
+def remove_punctuation(s):
+    return s.translate(str.maketrans('', '', string.punctuation))
+
+if __name__ == "__main__":
+    test = "hello, world!"
+    print("Reverse:", reverse_string(test))
+    print("Capitalize:", capitalize_words(test))
+    print("No Punctuation:", remove_punctuation(test))
